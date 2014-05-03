57d0ffabff5010f6b2204154a4968055107fe0fa
cloggy45 <cloggy45@googlemail.com>
2014-05-04 00:17:45 +0100

    Added validation rule for support model

--- app/Model/Support.php
+++ app/Model/Support.php
@@ -4,5 +4,27 @@ class Support extends AppModel
 {
     public $belongsTo = 'User';
 
+    public $validate = array(
+        'subject' => array(
+            'notEmpty' => array(
+                'rule' => 'notEmpty',
+                'message' => 'This field cannot be left blank'
+            ),
+            'maxLength' => array(
+                'rule' => array('maxLength', 30),
+                'message' => 'Subject can only have a maximum of 30 characters'
+            )
+        ),
+        'body' => array(
+            'notEmpty' => array(
+                'rule' => 'notEmpty',
+                'message' => 'This field cannot be left blank'
+            ),
+            'maxLength' => array(
+                'rule' => array('maxLength', 100),
+                'message' => 'Body can only have a maxmimum of 100 characters'
+            )
+        )
+    );
 
 }
